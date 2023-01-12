78c9a37afdadca8712fb8336751b90ff1cbd0c8e
erik814 <erikneafus@gmail.com>
2023-01-12 17:36:35 -0600

    Added the api. Put in javascript code to take a few categories from the api and display in my html

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,14 +1,26 @@
+let lat = 44.9479;
+let lon = -93.0873;
 
-var requestedAPI = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid='
+let currentCityName = document.querySelector('#currentCity');      // current city text area
+let currentTemp = document.querySelector('#currentTemp');          // current temp text area
+let currentHumidity = document.querySelector('#currentHumidity');  // current humidity text area
 
-function getAPI(){
-    fetch(requestedAPI)
-        .then(function(response){
-            return response.json();
-        })
-        .then(function(data){
-            console.log(data)
-        })
-        
-        
-}
+
+var requestedAPI = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=97a926960ee2c9606481892a903aa394`
+
+fetch(requestedAPI)
+    .then(response =>{
+        return response.json();
+    })
+    .then(data =>{
+        console.log(data)
+        console.log(data.city.name)
+        const cityName = data.city.name;          //selected city name
+        const temp = data.list[0].main.temp;      //selected city temp
+        const humidity = data.list[0].humidity;   //selected city humidity
+
+        currentCityName.textContent = cityName;
+        currentTemp.textContent = temp;
+        currentHumidity.textContent = humidity;
+
+    })
