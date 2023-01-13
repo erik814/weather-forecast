623cb1076db3a5cab2b2e76c499ea6aa8b965dc5
erik814 <erikneafus@gmail.com>
2023-01-12 18:09:46 -0600

    I added a kelvin to fahrenheit converter, and added a momentjs date to the current forecast.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,6 +6,13 @@ let currentTemp = document.querySelector('#currentTemp');          // current te
 let currentHumidity = document.querySelector('#currentHumidity');  // current humidity text area
 
 
+
+document.querySelector("#searchButton").addEventListener('click', function(e){
+    console.log("yes hello")
+});
+
+
+
 var requestedAPI = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=97a926960ee2c9606481892a903aa394`
 
 fetch(requestedAPI)
@@ -16,11 +23,12 @@ fetch(requestedAPI)
         console.log(data)
         console.log(data.city.name)
         const cityName = data.city.name;          //selected city name
-        const temp = data.list[0].main.temp;      //selected city temp
         const humidity = data.list[0].humidity;   //selected city humidity
+        const kelvin = data.list[0].main.temp;      //selected city kelvin temp
+        let temp = Math.floor(((kelvin-273.15)*1.8)+32);  //convert kelvin to fahrenheit
 
-        currentCityName.textContent = cityName;
-        currentTemp.textContent = temp;
+        currentCityName.textContent = `${cityName} ${moment().format('l')}`;
+        currentTemp.textContent = `Temp: ${temp}F`;
         currentHumidity.textContent = humidity;
 
     })
